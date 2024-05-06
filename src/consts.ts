0af09110d03d1b025d97698dedc7f5adca85bc8c
Pyunghyuk Yoo <yoophi@gmail.com>
2024-05-07 00:41:13 +0900

    design: minimal blog design

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '유병혁';
+export const SITE_DESCRIPTION = '이런 저런 이야기들';
