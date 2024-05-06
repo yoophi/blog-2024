5aa47c8c606db9571086efa74237b122e3da548b
Pyunghyuk Yoo <yoophi@gmail.com>
2024-05-07 02:16:59 +0900

    chore: fix back quote in code tag

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+    typography: {
+      default: {
+        css: {
+          code: {
+            '&::before': {
+              display: 'none',
+            },
+            '&::after': {
+              display: 'none',
+            },
+          }
+        },
+      },
+    },
 		extend: {},
 	},
 	plugins: [
